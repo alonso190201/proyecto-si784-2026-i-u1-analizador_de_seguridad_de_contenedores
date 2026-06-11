602e9a1648dd22ec8bf426659547da55b9fa2315
GerardoCLL <81579363+alonso190201@users.noreply.github.com>
2026-06-11 10:57:03 -0500

    fixe

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Startup script for Azure App Service Linux
-python -m gunicorn app:app --bind=0.0.0.0:8000 --workers=2 --timeout=120
+python -m gunicorn app:app --bind=0.0.0.0:${PORT:-8000} --workers=2 --timeout=120
+
